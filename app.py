217146f9269b33e7d69d80f3c31aaaf6a4126bdd
ashrafuldalif <ashrafulalif26@gmail.com>
2025-05-12 00:58:48 +0600

    problem : relode hoy na

--- app.py
+++ app.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template # type: ignore
 
 app = Flask(__name__)
+products=[
+    {"name":"monkey  D Luffy", "price":5000,   "pic":"../static/iamges/Cluffy.jpg"},
+    {"name":"Roronoa Zoro",  "price":1500,  "pic":"../static/iamges/Czoro.jpg" }
 
+]
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html",p=products)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+
+app.run(debug=True ,use_reloader=True)
+
